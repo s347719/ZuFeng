e8ed129d3566dd66f1aacb3145715a5d7af8d3e2
Mike shu <578076417@qq.com.com>
2015-07-29 15:46:26 +0800

    添加Fragment，搭建简单的布局

--- app/src/main/java/com/shuhuan/zufeng/app/MainActivity.java
+++ app/src/main/java/com/shuhuan/zufeng/app/MainActivity.java
@@ -1,19 +1,70 @@
 package com.shuhuan.zufeng.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.RadioGroup;
+import com.shuhuan.zufeng.app.fragments.CustmFragment;
+import com.shuhuan.zufeng.app.fragments.DiscoveryFragment;
+import com.shuhuan.zufeng.app.fragments.DownloadTingFragment;
+import com.shuhuan.zufeng.app.fragments.ProfileFragment;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private DiscoveryFragment discoveryFragment;
+    private CustmFragment customFrgment;
+    private DownloadTingFragment downloadTingFragment;
+    private ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        RadioGroup tabBar = (RadioGroup) findViewById(R.id.main_tab_bar);
+        tabBar.setOnCheckedChangeListener(this);
+        //默认选中第一个
+        tabBar.check(R.id.main_tab_item_discovery);
     }
 
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        FragmentManager manager  = getSupportFragmentManager();
 
+        FragmentTransaction transaction = manager.beginTransaction();
+        Fragment fragment = null;
+        switch (checkedId)
+        {
+            case R.id.main_tab_item_discovery:
+                if (discoveryFragment != null) {
+                    discoveryFragment = new DiscoveryFragment();
+                }
+                fragment= discoveryFragment;
+                break;
+            case R.id.main_tab_item_custom1:
+                if (customFrgment == null) {
+                    customFrgment = new CustmFragment();
+                }
+                fragment= customFrgment;
+                break;
+            case R.id.main_tab_item_download:
+                if (downloadTingFragment == null) {
+                    downloadTingFragment = new DownloadTingFragment();
+                }
+                fragment= downloadTingFragment;
+                break;
+            case R.id.main_tab_item_profile:
+                if (profileFragment == null) {
+                    profileFragment = new ProfileFragment();
+                }
+                fragment = profileFragment;
+                break;
+        }
+
+        transaction.replace(R.id.main_fragment_container,fragment);
+        transaction.commit();
+
+    }
 }
---
+++ app/src/main/java/com/shuhuan/zufeng/app/fragments/CustmFragment.java
@@ -0,0 +1,29 @@
+package com.shuhuan.zufeng.app.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.shuhuan.zufeng.app.R;
+
+/**
+ * Created with Intellij IDEA.
+ * Project:ZuFeng
+ * user:MIKE Shu
+ * Email:578076417@qq.com
+ * Created on 2015/7/29.
+ */
+public class CustmFragment extends Fragment {
+
+    public CustmFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_custom, container, false);
+    }
+}
