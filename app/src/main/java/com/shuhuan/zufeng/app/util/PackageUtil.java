3665f211b4220bb1ab3ca7b2d119a7b40fd427f9
Mike shu <578076417@qq.com.com>
2015-07-29 10:04:36 +0800

    加载包工具类

---
+++ app/src/main/java/com/shuhuan/zufeng/app/util/PackageUtil.java
@@ -0,0 +1,22 @@
+package com.shuhuan.zufeng.app.util;
+
+/**
+ * Created with Intellij IDEA.
+ * Project:ZuFeng
+ * user:MIKE Shu
+ * Email:578076417@qq.com
+ * Created on 2015/7/29.
+ */
+
+/**
+ *
+ * 软件包工具类，获取版本号信息
+ */
+public class PackageUtil {
+
+    private PackageUtil()
+    {
+
+    }
+
+}
